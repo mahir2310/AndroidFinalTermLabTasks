2e00f212222bb8bc6daf9e99d8b4bc6d3ce9de65
mahir <sajidhasanmahir2003@gmail.com>
2026-05-08 02:26:04 +0600

    chore: add firebase auth gradle wiring

---
+++ StudentAuthApp/build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
