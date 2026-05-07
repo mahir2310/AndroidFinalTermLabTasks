b473b9c4fa8a36d71372e75762803912bd7056e5
mahir <sajidhasanmahir2003@gmail.com>
2026-05-08 01:28:49 +0600

    chore: rename app package and register all activities

--- UserSettingsApp/app/build.gradle.kts
+++ UserSettingsApp/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.usersettingsapp"
+    namespace = "com.university.usersettings"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.usersettingsapp"
+        applicationId = "com.university.usersettings"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
